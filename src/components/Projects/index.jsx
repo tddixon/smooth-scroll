c563ded40198436878abf8821a7220700b966e48
Trevor Dixon <81465964+tddixon@users.noreply.github.com>
2023-07-17 18:48:40 +0700

    bryant2

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,72 +1,81 @@
-import React, { useState, useLayoutEffect, useRef } from 'react'
-import styles from './style.module.css';
-import Image from 'next/image';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useState, useLayoutEffect, useRef } from "react";
+import styles from "./style.module.css";
+import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const projects = [
-    {
-        title: "Salar de Atacama",
-        src: "salar_de_atacama.jpg"
-    },
-    {
-        title: "Valle de la luna",
-        src: "valle_de_la_muerte.jpeg"
-    },
-    {
-        title: "Miscanti Lake",
-        src: "miscani_lake.jpeg"
-    },
-    {
-        title: "Miniques Lagoons",
-        src: "miniques_lagoon.jpg"
-    },
-]
+  {
+    title: "THE ESTATE",
+    src: "slide-3.jpeg",
+  },
+  {
+    title: "OUR COLLECTION",
+    src: "wine-cover.jpg",
+  },
+  {
+    title: "BRYANT IMPORTS",
+    src: "miscani_lake.jpeg",
+  },
+];
 
 export default function Index() {
+  const [selectedProject, setSelectedProject] = useState(0);
+  const container = useRef(null);
+  const imageContainer = useRef(null);
 
-    const [selectedProject, setSelectedProject] = useState(0);
-    const container = useRef(null);
-    const imageContainer = useRef(null);
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.create({
+      trigger: imageContainer.current,
+      pin: true,
+      start: "top-=100px",
+      end: document.body.offsetHeight - window.innerHeight - 50,
+    });
+  }, []);
 
-    useLayoutEffect( () => {
-        gsap.registerPlugin(ScrollTrigger);
-        ScrollTrigger.create({
-            trigger: imageContainer.current,
-            pin: true,
-            start: "top-=100px",
-            end: document.body.offsetHeight - window.innerHeight - 50,
-        })
-    }, [])
-
-    return (
-        <div ref={container} className={styles.projects}>
-            <div className={styles.projectDescription}>
-                <div ref={imageContainer} className={styles.imageContainer}>
-                    <Image 
-                        src={`/images/${projects[selectedProject].src}`}
-                        fill={true}
-                        alt="project image"
-                        priority={true}
-                    />
-                </div>
-                <div className={styles.column}>
-                    <p>The flora is characterized by the presence of high elevation wetland, as well as yellow straw, broom sedge, tola de agua and tola amaia.</p>
-                </div>
-                <div className={styles.column}>
-                    <p>Some, like the southern viscacha, vicuña and Darwins rhea, are classified as endangered species. Others, such as Andean goose, horned coot, Andean gull, puna tinamou and the three flamingo species inhabiting in Chile (Andean flamingo, Chilean flamingo, and Jamess flamingo) are considered vulnerable.</p>
-                </div>
-            </div>
-
-            <div className={styles.projectList}>
-                {
-                    projects.map( (project, index) => {
-                        return <div key={index} onMouseOver={() => {setSelectedProject(index)}} className={styles.projectEl}>
-                            <h2>{project.title}</h2>
-                        </div>
-                    })
-                }
-            </div>
+  return (
+    <div ref={container} className={styles.projects}>
+      <div className={styles.projectDescription}>
+        <div ref={imageContainer} className={styles.imageContainer}>
+          <Image
+            src={`/images/${projects[selectedProject].src}`}
+            fill={true}
+            alt="project image"
+            priority={true}
+          />
         </div>
-    )
+        <div className={styles.column}>
+          <p>
+            The soil composition, the sun-kissed slopes, and the cooling breezes
+            all leave their imprint on our grapes, creating a symphony of
+            flavors and textures. sunset
+          </p>
+        </div>
+        <div className={styles.column}>
+          <p>
+            A wine is the accumulation of incalculable little brushstroke
+            nuances. While there are an infinite number of potentialities that
+            come out of small choices, it is intention behind them that matters.
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.projectList}>
+        {projects.map((project, index) => {
+          return (
+            <div
+              key={index}
+              onMouseOver={() => {
+                setSelectedProject(index);
+              }}
+              className={styles.projectEl}
+            >
+              <h2>{project.title}</h2>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
